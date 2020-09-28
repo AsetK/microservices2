463bf8cef5ddc117f4379634814c36aa760ca3b6
Asset Kenezhanov <a.kenezhanov@consaltica.ru>
2020-09-28 21:34:29 +0600

    8 - call currency-exchange-service via RestTemplate

--- currency-conversion-service/src/main/java/com/example/microservices2/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices2/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,11 +3,14 @@ package com.example.microservices2.currencyconversionservice.controller;
 import com.example.microservices2.currencyconversionservice.entity.CurrencyConversionBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -21,8 +24,24 @@ public class CurrencyConversionController {
                                                   @PathVariable BigDecimal quantity) {
         Integer port = Integer.valueOf(environment.getProperty("server.port"));
 
-        return new CurrencyConversionBean(1L, from, to,
-                BigDecimal.valueOf(75L), quantity, BigDecimal.valueOf(750), port);
+        ResponseEntity<CurrencyConversionBean> response = callCurrencyExchangeService(from, to);
 
+        CurrencyConversionBean body = response.getBody();
+        BigDecimal conversionMultiple = body.getConversionMultiple();
+
+        return new CurrencyConversionBean(1L, from, to,
+                conversionMultiple, quantity, quantity.multiply(conversionMultiple), port);
+
+    }
+
+    private ResponseEntity<CurrencyConversionBean> callCurrencyExchangeService(String from, String to) {
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> response =
+                new RestTemplate().getForEntity("http://localhost:8000//currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversionBean.class, uriVariables);
+        return response;
     }
 }
